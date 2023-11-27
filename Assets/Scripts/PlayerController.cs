7da1a078b0118ba8abccb01565dd1fc2f1df288c
Fakhar Latif <118841796+ptclbb123@users.noreply.github.com>
2023-11-27 22:20:50 +0500

    player Movemnt setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject viewPoint;
     [SerializeField] private float mouseSensitivity = 1f;
     [SerializeField] private float verticalRotationStore;
+    [SerializeField] private bool invertLook;
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector3 moveDir, movement;
     private Vector2 mouseInput;
 
     // Start is called before the first frame update
     private void Start()
     {
         viewPoint = this.transform.GetChild(1).gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -22,6 +26,17 @@ public class PlayerController : MonoBehaviour
         this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
         verticalRotationStore += mouseInput.y;
         verticalRotationStore = Mathf.Clamp(verticalRotationStore, -60f, 60f);
-        viewPoint.transform.rotation = Quaternion.Euler(-verticalRotationStore, viewPoint.transform.rotation.eulerAngles.y, viewPoint.transform.rotation.eulerAngles.z);
+        if (invertLook)
+        {
+            viewPoint.transform.rotation = Quaternion.Euler(verticalRotationStore, viewPoint.transform.rotation.eulerAngles.y, viewPoint.transform.rotation.eulerAngles.z);
+        }
+        else
+        {
+            viewPoint.transform.rotation = Quaternion.Euler(-verticalRotationStore, viewPoint.transform.rotation.eulerAngles.y, viewPoint.transform.rotation.eulerAngles.z);
+        }
+
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
+        this.transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
